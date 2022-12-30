04f507ece9d3467f5b2b8664d1dde316dfe9be9a
Iaarun <automationbyaarun@gmail.com>
2022-12-30 07:54:23 +0530

    basic code

---
+++ src/testPackage/HelloWorld.java
@@ -0,0 +1,36 @@
+package testPackage;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+
+		
+		//Single line comment
+		
+		/*
+		 multi
+		 line 
+		 comment
+		 */
+		System.out.println("Hello World!!");
+		
+		int number = 20;
+		System.out.println(number);
+		
+		String name = "Arun";
+		System.out.println("Hello "+name);
+		
+		System.out.println("Hello World!!");
+	}
+	
+	/**
+	 @author Arun
+	 This us testmethod to understand the
+	 comment process and documentation
+	 */
+	
+	public static void test() {
+		System.out.println("This is a test method");
+	}
+
+}
---
+++ src/testPackage/TestClass.java
@@ -0,0 +1,7 @@
+package testPackage;
+
+public class TestClass {
+	 public static void main(String[] args) {
+		
+	}
+}
